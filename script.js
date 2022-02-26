9b5bcee7633325a4b9cbe32704c8eb6141f115dd
Gina Henderson <gina@ginahenderson.me>
2022-02-26 06:13:33 -0600

    Create arithmetic functions

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const clickUp = new Audio('sounds/click-up.mp3');
 const mute = document.getElementById('muted');
 const muteContainer = document.querySelector('.mute');
 const speaker = document.querySelector('.fas');
+const screen = document.querySelector('.screen');
 
 mute.addEventListener('change', fadeIcon);
 function fadeIcon() {
@@ -22,14 +23,62 @@ function fadeIcon() {
     }
 }
 
-button.forEach(a=> {
+button.forEach(a => {
     a.addEventListener('mousedown', playClick);
     a.addEventListener('mouseup', playClick);
 });
 
 function playClick(e) {
-    console.log(mute.checked);
     if (mute.checked) return;
     if (e.type == 'mousedown') clickDown.play();
     else if (e.type == 'mouseup') clickUp.play();
 }
+
+
+document.querySelectorAll('.num-key').forEach(a => {
+    a.addEventListener('click', logDigit.bind(a))
+})
+
+// Convert button clicks to digits
+
+let enteredDigits = [];
+
+function logDigit() {
+    enteredDigits.push(parseInt(this.innerText));
+    console.log(enteredDigits);
+}
+
+// Add, subtract, multiply, divide
+
+function add(arr) {
+    let x = arr[0];
+    let y = arr[1];
+    return x + y; 
+}
+
+function subtract(arr) {
+    let x = arr[0];
+    let y = arr[1];
+    return x - y;
+}
+
+function multiply(arr) {
+    let x = arr[0];
+    let y = arr[1];
+    return x * y;
+}
+
+function divide(arr) {
+    let x = arr[0];
+    let y = arr[1];
+    return x / y;
+}
+
+// Operate function takes operator and 2 numbers, calls function on them
+function operate(arr, operator) {
+
+}
+
+// Function populates display on button click
+// Operate: store number, store operator, call function on "="
+// Show answer in display
