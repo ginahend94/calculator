b951a09a7ac4c43d4aa2f8fb9dde902772d16b52
Gina Henderson <gina@ginahenderson.me>
2022-03-01 07:20:36 -0600

    Fix bugs, add kb support

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const button = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button');
 const clickDown = new Audio('sounds/click-down.mp3');
 const clickUp = new Audio('sounds/click-up.mp3');
 const mute = document.getElementById('muted');
 const muteContainer = document.querySelector('.mute');
 const speaker = document.querySelector('.fas');
-const screen = document.querySelector('.screen');
 
+// UI Elements
 mute.addEventListener('change', fadeIcon);
 function fadeIcon() {
     if (mute.checked == true) {
-        console.log('muted');
         speaker.classList.add('fa-volume-mute');
         speaker.classList.remove('fa-volume-up');
         speaker.style = 'color:rgba(0,0,0,.3);'
         muteContainer.setAttribute('title', 'Sound off')
     } else {
-        console.log('unmuted');
         speaker.classList.add('fa-volume-up');
         speaker.classList.remove('fa-volume-mute');
         speaker.style = 'color:inherit;'
@@ -23,56 +21,93 @@ function fadeIcon() {
     }
 }
 
-button.forEach(a => {
+buttons.forEach(a => {
     a.addEventListener('mousedown', playClick);
     a.addEventListener('mouseup', playClick);
 });
 
 function playClick(e) {
     if (mute.checked) return;
-    if (e.type == 'mousedown') clickDown.play();
-    else if (e.type == 'mouseup') clickUp.play();
+    if (e.type == 'mousedown' || e.type == 'keydown') clickDown.play();
+    else if (e.type == 'mouseup' || e.type == 'keyup') clickUp.play();
 }
 
+// Calculator code
 
-// Convert button clicks to digits
-const numKey = document.querySelectorAll('.num-key');
-numKey.forEach(a => {
-    a.addEventListener('click', logDigit.bind(a));
-})
-
-let enteredDigits = [];
-
-function logDigit() {
-    enteredDigits.push(parseInt(this.innerText));
-    console.log(enteredDigits);
-}
-
-const operatorKey = document.querySelectorAll('.operator-key');
-operatorKey.forEach(a => {
-    a.addEventListener('click', logOperator.bind(a));
-})
-
-// Press key, stored in num array
-// Press operator, stored in var
-
+const screen = document.querySelector('.screen');
+let currentNum = '';
 let numbers = [];
-
 let operator = '';
-function logOperator() {
-    operator = this.classList[0];
-    numbers.push(parseInt(enteredDigits.join('')));
-    console.log(operator);
-    console.log(numbers);
-    enteredDigits = [];
+let answer = 0;
+
+
+// Function to take in numbers
+// turn key presses into digits
+document.querySelectorAll('.num-key').forEach(a => a.addEventListener('click', logDigit));
+function logDigit(e) {
+    // Check if equals was pressed, clears numbers
+    if (pressedEquals && !operator) {
+        clearNumbers();
+        pressedEquals = false;
+    }
+    // Clear highlighted operator on next key press
+    removeActiveOperator();
+    // Include decimal
+    if (e.target.value == '.' && currentNum.indexOf('.') >= 0) return;
+    // Cancel if too long    
+    if (screen.offsetWidth >= 170) return showTooLongPopup();
+    updateScreen(currentNum);
+    // turn digits into numbers
+    currentNum += e.target.value;
+    // Display on screen
+    if (currentNum == '.') currentNum = '0.';
+    updateScreen(currentNum);
+}
+function updateScreen(text) {
+    let screenText = text;
+    screen.innerText = screenText;
+}
+function showTooLongPopup() {
+    document.querySelector('.too-long-popup').classList.add('show-popup');
+    setTimeout(() => {
+        document.querySelector('.too-long-popup').classList.remove('show-popup');
+    }, 1000);
 }
 
+// Function to take operator
+const operatorKeys = document.querySelectorAll('.operator-key');
+operatorKeys.forEach(a => a.addEventListener('click', setOperator));
+
+// turn key press into operation
+function setOperator(e) {
+    if (!currentNum && currentNum != '0' && !numbers.length) return;
+    setNumbers();
+    // Find answer, then set new operator
+    if (numbers.length >= 2) operate(e);
+    operator = e.target.value;
+    // Highlight active operator
+    removeActiveOperator();
+    e.target.classList.add('active-operator');
+}
+function removeActiveOperator() {
+    operatorKeys.forEach(a => a.classList.remove('active-operator'));
+}
+function setNumbers() {
+    if (!currentNum && currentNum != '0') return;
+    numbers.push(parseFloat(currentNum));
+    currentNum = '';
+}
+function clearNumbers() {
+    numbers = [];
+}
+// Function to calculate from two numbers and operator
 // Add, subtract, multiply, divide
+document.querySelector('.equals').addEventListener('click', operate);
 
 function add(arr) {
     let x = arr[0];
     let y = arr[1];
-    return x + y; 
+    return x + y;
 }
 
 function subtract(arr) {
@@ -93,10 +128,155 @@ function divide(arr) {
     return x / y;
 }
 
-// Operate function takes operator and 2 numbers, calls function on them
-// Function populates display on button click
-// Operate: store number, store operator, call function on "="
-// Show answer in display
+let divideByZero = 0;
+let pressedEquals = false;
 
-// Press key, stored in num2 array
-// Press enter, call operate()
+function operate(e) {
+    if (numbers.length < 1) return;
+    setNumbers();
+    if (numbers.length < 2) return;
+    switch (operator) {
+        case '+':
+            answer = add(numbers);
+            break;
+        case '-':
+            answer = subtract(numbers);
+            break;
+        case '*':
+            answer = multiply(numbers);
+            break;
+        case '/':
+            if (numbers[1] == 0) {
+                updateScreen('No can do, babes.');
+                divideByZero++;
+                if (divideByZero >= 3) {
+                    updateScreen('Seriously? Come on, pal.');
+                }
+                if (divideByZero == 42) updateScreen('Persistent, aren\'t we?');
+                return setTimeout(allClear, 1000);
+            }
+            answer = divide(numbers);
+            break;
+        default:
+            console.log('no operator');
+            return;
+    }
+// set answer as new number 1
+    currentNum = answer;
+// Function to display answer on screen
+    updateScreen(currentNum);
+    clearNumbers();
+    setNumbers();
+    currentNum = '';
+    if (e.target.value == 'equals') {
+        pressedEquals = true;
+        operator = null;
+    }
+}
+
+// Set up fn keys
+    // % => divide current number by 100
+document.querySelector('.percent').addEventListener('click', percent);
+function percent() {
+    if (!currentNum) return;
+    currentNum /= 100;
+    updateScreen(currentNum);
+}
+    // sqrt => math sqrt on current number
+document.querySelector('.sqrt').addEventListener('click', sqrt);
+function sqrt() {
+    if (!currentNum) return;
+    currentNum = Math.sqrt(currentNum);
+    updateScreen(currentNum);
+}
+    //pi 
+document.querySelector('.pi').addEventListener('click', pi);
+function pi() {
+    currentNum = Math.PI;
+    updateScreen(currentNum);
+}
+
+    // delete => slice screen contents
+document.querySelector('.delete').addEventListener('click', backspace);
+function backspace() {
+    currentNum = currentNum.slice(0, -1);
+    if (!currentNum)  return updateScreen('0');
+    updateScreen(currentNum);
+}
+    // clear screen
+document.querySelector('.clear').addEventListener('click', clearScreen);
+document.querySelector('.all-clear').addEventListener('click', allClear);
+
+function clearScreen() {
+    currentNum = '';
+    updateScreen('0');
+}
+function allClear() {
+    currentNum = '';
+    updateScreen('0');
+    clearNumbers();
+    operator = null;
+    removeActiveOperator();
+}
+
+// Allow keyboard input
+document.addEventListener('keydown', registerKeyboard);
+document.addEventListener('keyup', registerKeyboard);
+
+function registerKeyboard(e) {
+    // Find corresponding key on calculator
+    const findKey = query => {
+       return Array
+            .from(document.querySelectorAll(query))
+            .find(a => a.value == e.key);
+    }
+    // Number keys
+    if (e.keyCode >= 48 && e.keyCode <= 57 
+        || e.keyCode >= 96 && e.keyCode <= 105) {
+            if (e.repeat) return; // Prevent press-and-hold spamming
+            const target = findKey('.num-key');
+            const event = {type: 'keydown', target};
+            simulateClick(e, target);
+            if (e.type == 'keyup') return;
+            logDigit(event);
+    }
+    // Operation keys
+    const operationKeyCodes = ['+','-','*','/'];
+    if (operationKeyCodes.includes(e.key)) {
+        if (e.repeat) return;
+        const target = findKey('.operator-key');
+        const event = {type:'keydown', target};
+        simulateClick(e, target);
+        if (e.type == 'keyup') return;
+        setOperator(event);
+    }
+    if (e.key == '=' || e.key == 'Enter') {
+        if (e.repeat) return;
+        const equals = document.querySelector('.equals');
+        const event = {type:'keydown', target:equals};
+        simulateClick(e, equals);
+        if (e.type == 'keyup') return;
+        operate(event);
+    }
+    if (e.key == 'Backspace') {
+        if (e.repeat) return;
+        simulateClick(e, document.querySelector('.delete'));
+        if (e.type == 'keyup') return;
+        backspace();
+    }
+    if (e.key == 'Escape') {
+        if (e.repeat) return;
+        simulateClick(e, document.querySelector('.all-clear'));
+        if (e.type == 'keyup') return;
+        allClear();
+    }
+}
+function simulateClick(e, target) {
+    if (e.repeat) return;
+    if (e.type == 'keydown') {
+        target.classList.add('button-active');
+    } else if (e.type == 'keyup') {
+        target.classList.remove('button-active');
+    }
+    playClick(e);
+}
